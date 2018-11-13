56eade582b250c450f1792f558bc7e502d4e8deb
Andreas Haessler <andreas.haessler@outlook.at>
2018-11-13 09:20:08 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+window.onload = () => {
+    const ordnerList = document.getElementById('ordner')
+
+    fetch('/klassenordner.json').then(res => {
+        res.json().then(data => {
+            const entries = parseEntries(data)
+            const current = getEntryForDate(entries, new Date())
+
+            if (current === null) {
+                const li = document.createElement('li')
+                li.innerText = "keine"
+                ordnerList.appendChild(li)
+                return
+            }
+
+            const ordners = current.ordner.map(ordner => {
+                const li = document.createElement('li')
+                li.innerText = ordner
+                return li
+            })
+
+            for (let ordner of ordners) {
+                ordnerList.appendChild(ordner)
+            }
+        })
+    })
+}
+
+function parseEntries(data) {
+    return data.map(entry => ({
+        ...entry,
+        start: new Date(entry.start),
+        end: new Date(entry.end)
+    }))
+}
+
+function getEntryForDate(entries, date) {
+    for (let entry of entries) {
+        if (entry.start <= date && entry.end >= date) {
+            return entry
+        }
+    }
+    return null
+}
